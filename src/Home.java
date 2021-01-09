ac0cc0408b92e919b095f3c64f01574770ebf67b
Marisabel <72731147+immarisabel@users.noreply.github.com>
2021-01-09 13:51:19 +0100

    wip

--- src/Home.java
+++ src/Home.java
@@ -1,23 +1,26 @@
 public class Home {
 
-static String name = null;
-static String date = null;
-static int PIN = 0000;
+//static String name = null;
+//static String date = null;
+//static int PIN = 0000;
     public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("org.sqlite.JDBC");
 
-        System.out.println("\nWelcome to the journal of " + name +"!\n");
+        Login f = new Login();
 
-
-        System.out.println("To unlock, please type in your PIN number.\n");
-
-        while(PIN==TRUE) {
-            MainScreen.Hello();
-        }
-        {
-            System.out.println("Sorry, wrong PIN.");
-            //LOOP BACK (see game loop)
-        }
+//        Class.forName("org.sqlite.JDBC");
+//
+//        System.out.println("\nWelcome to the journal of " + name +"!\n");
+//
+//
+//        System.out.println("To unlock, please type in your PIN number.\n");
+//
+//        while(PIN==TRUE) {
+//            MainScreen.Hello();
+//        }
+//        {
+//            System.out.println("Sorry, wrong PIN.");
+//            //LOOP BACK (see game loop)
+//        }
 
 
 
