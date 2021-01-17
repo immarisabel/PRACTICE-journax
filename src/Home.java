34e4b10705c5ec43e4c7c057315652f93f1ba5ad
Marisabel <72731147+immarisabel@users.noreply.github.com>
2021-01-17 14:57:02 +0100

    WIP

--- src/Home.java
+++ src/Home.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.CharBuffer;
-import java.util.Arrays;
-import java.util.Scanner;
+import javax.swing.*;
+
 
 public class Home {
 
@@ -11,24 +7,22 @@ public class Home {
     public static void main(String[] args) {
 
 
+
+
+
+
+
+
+
+
+
+
+
+        /*DO NOT TOUCH*/
+
         Date journalDate = new Date();
         System.out.println("\n" + journalDate.today());
 
-        Scanner scanner = new Scanner(System.in);
-        String title = scanner.nextLine();
-        String text = scanner.nextLine();
-
-
-        try (InputStreamReader in = new InputStreamReader(System.in);
-             BufferedReader buffer = new BufferedReader(in)) {
-            String line;
-            while ((line = buffer.readLine()) != null) {
-                String[] tokens = line.split("\\s");
-                System.out.println(Arrays.toString(tokens));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         System.out.println(">>>>>SAVED<<<<<");
 
     }
