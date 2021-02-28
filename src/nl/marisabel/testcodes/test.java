d96bd823b6076efac64eddc69e8be6c4e5ce064d
Marisabel <72731147+immarisabel@users.noreply.github.com>
2021-02-28 19:51:36 +0100

    tests

---
+++ src/nl/marisabel/testcodes/test.java
@@ -0,0 +1,9 @@
+package nl.marisabel.testcodes;
+
+public class test {
+
+    public static void main(String[] args) {
+        Runnable r = () -> System.out.println("hello");
+        //Object created and assigned to r
+    }
+}
